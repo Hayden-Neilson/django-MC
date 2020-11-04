869f782aa2bd54491a76a982d3de772f3888fea2
Hayden-Neilson <haydenneilson5@gmail.com>
2020-11-04 06:26:45 -0700

    added another view

--- food/views.py
+++ food/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hey peeps!")
+
+def items(request):
+    return HttpResponse("Hey items!")
